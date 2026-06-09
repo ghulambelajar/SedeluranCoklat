e0d9d04f5265b9fc0e381e2d8a2584b66cfa9f4c
ghulambelajar <nilamsekuat@gmail.com>
2026-06-09 13:56:01 +0700

    feat: membuat endpoint login dan implementasi jsonwebtoken (JWT)

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
   try {
@@ -43,6 +44,50 @@ exports.register = async (req, res) => {
     });
   } catch (error) {
     console.error("Error saat register:", error);
+    res
+      .status(500)
+      .json({ message: "Terjadi kesalahan pada server", error: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email dan kata sandi wajib diisi" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Email atau kata sandi salah" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password_hash);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Email atau kata sandi salah" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" },
+    );
+
+    res.status(200).json({
+      message: "Login berhasil!",
+      token: token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    console.error("Error saat login:", error);
     res
       .status(500)
       .json({ message: "Terjadi kesalahan pada server", error: error.message });
